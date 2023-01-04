376f6d0f554abf7ef453647a6162158e03b47148
colthreepv <valerio.coltre@gmail.com>
2023-01-04 20:56:26 +0100

    wip: spend coins

--- packages/backend/src/user-account/user-account.service.spec.ts
+++ packages/backend/src/user-account/user-account.service.spec.ts
@@ -1,5 +1,88 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { UserAccountService } from './user-account.service'
+import { Coins } from 'shared-types/src/crud'
+import { fastSpendCoins, simpleSpendCoins, spendCoins, UserAccountService } from './user-account.service'
+
+describe('UserAccountService support functions', () => {
+  it('given a set amount of coins deposit, user spend an amount that can generate a rest with its given coins', async () => {
+    const coins: Coins = {
+      '5': 4, // 5 * 4 = 20
+      '10': 0,
+      '20': 2, // 20 * 2 = 40
+      '50': 0,
+      '100': 1, // 100 * 1 = 100
+    } // 160
+
+    // REST solution could be
+    // (a) { '5': 3, '10': 0, '20': 1, '50': 0, '100': 0 }
+    // (b) { '5': 1, '10': 1, '20': 1, '50': 0, '100': 0 }
+    // but I would prefer (a), so fastSpendCoins does that
+    const { partialRest, remaining } = fastSpendCoins(coins, 125)
+
+    expect(partialRest['5']).toBe(3)
+    expect(partialRest['10']).toBe(0)
+    expect(partialRest['20']).toBe(1)
+    expect(partialRest['50']).toBe(0)
+    expect(partialRest['100']).toBe(0)
+    expect(remaining).toBe(0)
+  })
+
+  it('given a set amount of coins deposit, user spend an amount that needs another iteration to generate a proper rest', async () => {
+    const coins: Coins = {
+      '5': 2, // 10
+      '10': 0,
+      '20': 0,
+      '50': 1, // 50
+      '100': 1, // 100
+    } // 160
+    const { partialRest, remaining } = fastSpendCoins(coins, 115)
+
+    expect(partialRest['5']).toBe(0)
+    expect(partialRest['10']).toBe(0)
+    expect(partialRest['20']).toBe(0)
+    expect(partialRest['50']).toBe(1)
+    expect(partialRest['100']).toBe(0)
+    expect(remaining).toBe(5)
+  })
+
+  it.only('user spend an amount that needs another iteration to generate a proper rest', () => {
+    const coins: Coins = {
+      '5': 2, // 10
+      '10': 0,
+      '20': 0,
+      '50': 1, // 50
+      '100': 1, // 100
+    } // 160
+    const { partialRest, remaining } = fastSpendCoins(coins, 115)
+
+    expect(partialRest['5']).toBe(0)
+    expect(partialRest['10']).toBe(0)
+    expect(partialRest['20']).toBe(0)
+    expect(partialRest['50']).toBe(1)
+    expect(partialRest['100']).toBe(0)
+    expect(remaining).toBe(5)
+
+    const restCoins = simpleSpendCoins(partialRest, remaining)
+    expect(restCoins['5']).toBe(1)
+    expect(restCoins['20']).toBe(2)
+  })
+
+  it('same case as before but with spendCoins', () => {
+    const coins: Coins = {
+      '5': 2, // 10
+      '10': 0,
+      '20': 0,
+      '50': 1, // 50
+      '100': 1, // 100
+    } // 160
+    const restCoins = spendCoins(coins, 115)
+    console.log({ restCoins })
+    expect(restCoins['5']).toBe(1)
+    expect(restCoins['10']).toBe(0)
+    expect(restCoins['20']).toBe(2)
+    expect(restCoins['50']).toBe(0)
+    expect(restCoins['100']).toBe(0)
+  })
+})
 
 describe('UserAccountService', () => {
   let service: UserAccountService
@@ -10,9 +93,27 @@ describe('UserAccountService', () => {
     }).compile()
 
     service = module.get<UserAccountService>(UserAccountService)
+
+    await service.createAccount('user1')
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
   })
+
+  it('given a set of coins, user can deposit them', async () => {
+    const coins: Coins = {
+      '5': 4, // 5 * 4 = 20
+      '10': 0,
+      '20': 2, // 20 * 2 = 40
+      '50': 0,
+      '100': 1, // 100 * 1 = 100
+    } // 160
+    await service.depositAccount('user1', coins)
+
+    const returnCoins = await service.checkAccount('user1')
+    expect(returnCoins).toEqual(coins)
+  })
+
+  // it('given a previous deposit, user can spend coins', async () => {
 })
--- packages/backend/src/user-account/user-account.service.ts
+++ packages/backend/src/user-account/user-account.service.ts
@@ -3,6 +3,14 @@ import { Coins } from 'shared-types/src/crud'
 
 const VMAccounts = new Map<string, Coins>()
 
+const unlimitedVault: Coins = {
+  '5': 999,
+  '10': 999,
+  '20': 999,
+  '50': 999,
+  '100': 999,
+}
+
 const freshAccount = (): Coins => ({
   '5': 0,
   '10': 0,
@@ -13,15 +21,85 @@ const freshAccount = (): Coins => ({
 
 export const fromCoinsToNumber = (coins: Coins): number => {
   return (
-    (coins['5'] ?? 0 * 5) +
-    (coins['10'] ?? 0 * 10) +
-    (coins['20'] ?? 0 * 20) +
-    (coins['50'] ?? 0 * 50) +
-    (coins['100'] ?? 0 * 100)
+    (coins['5'] ?? 0) * 5 +
+    (coins['10'] ?? 0) * 10 +
+    (coins['20'] ?? 0) * 20 +
+    (coins['50'] ?? 0) * 50 +
+    (coins['100'] ?? 0) * 100
   )
 }
 
-// export const spendCoins = (coins: Coins, amount: number): Coins => {}
+export const sumCoins = (coin0: Coins, coin1: Coins) => {
+  const newCoins = freshAccount()
+  for (const coinValue of [100, 50, 20, 10, 5]) {
+    newCoins[coinValue] = Number(coin0[coinValue] ?? 0) + Number(coin1[coinValue] ?? 0)
+  }
+  return newCoins
+}
+
+// tries to spend the coins matching them by coinvalue, top -> bottom
+export const fastSpendCoins = (coins: Coins, amount: number): { partialRest: Coins; remaining: number } => {
+  let remaining = amount
+  const partialRest = freshAccount()
+
+  for (const coinValue of [100, 50, 20, 10, 5]) {
+    const availableCoins = coins[coinValue] ?? 0
+    if (availableCoins === 0) continue
+
+    const amountTaken = Math.min(availableCoins, Math.floor(remaining / coinValue))
+    console.log({
+      coinValue,
+      availableCoins,
+      amountTaken,
+      val: Math.floor(remaining / coinValue),
+    })
+    remaining -= amountTaken * coinValue
+    partialRest[coinValue] = availableCoins - amountTaken
+    if (remaining <= 0) break
+  }
+
+  return { partialRest, remaining }
+}
+
+export const restFromVault = (vault: Coins, rest: number): Coins => {
+  const newCoins = freshAccount()
+  let remaining = rest
+
+  for (const coinValue of [100, 50, 20, 10, 5]) {
+    const availableCoins = vault[coinValue] ?? 0
+    if (availableCoins === 0) continue
+
+    const amountTaken = Math.min(availableCoins, Math.floor(remaining / coinValue))
+    remaining -= amountTaken * coinValue
+    newCoins[coinValue] = amountTaken
+    if (remaining <= 0) break
+  }
+
+  return newCoins
+}
+
+// from a Coin object spend a specified amount and return the leftover coins
+export const simpleSpendCoins = (coins: Coins, amount: number): Coins => {
+  const total = fromCoinsToNumber(coins)
+  if (total < amount) {
+    throw new Error('Not enough coins')
+  }
+
+  const restToGive = total - amount
+  return restFromVault(unlimitedVault, restToGive)
+}
+
+// returns the rest, or an error
+export const spendCoins = (coins: Coins, amount: number): Coins => {
+  const total = fromCoinsToNumber(coins)
+  if (total < amount) {
+    throw new Error('Not enough coins')
+  }
+  const { partialRest: newCoins, remaining } = fastSpendCoins(coins, amount)
+
+  if (remaining === 0) return newCoins
+  return sumCoins(newCoins, restFromVault(unlimitedVault, remaining))
+}
 
 @Injectable()
 export class UserAccountService {
@@ -44,5 +122,14 @@ export class UserAccountService {
 
     VMAccounts.set(username, nextAccount)
     return nextAccount
+  }
+
+  async checkAccount(username: string): Promise<Coins> {
+    const account = VMAccounts.get(username)
+    if (account == null) {
+      throw new Error('Account not found')
+    }
+
+    return account
   }
 }
